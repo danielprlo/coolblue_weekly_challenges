900cf8b764abf1fef9de37cc4468e80378b1a718
Dani <daniel.perez@coolblue.nl>
2022-11-01 09:42:28 +0100

    Do up to 100 Fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,11 +5,11 @@ def not_using_mod():
     start_time = time.time()
     three = 3
     five = 5
-    result = list(range(1, 100))
-    while three < 100:
+    result = list(range(1, 101))
+    while three <= 100:
         result[three - 1] = 'Fizz'
         three += 3
-    while five < 100:
+    while five <= 100:
         result[five - 1] = 'FizzBuzz' if result[five - 1] == 'Fizz' else 'Buzz'
         five += 5
 
