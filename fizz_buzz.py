8b4cd65ae0882635d22fd808ebc29c60af63de8b
Dani <daniel.perez@coolblue.nl>
2022-11-01 10:15:02 +0100

    set constant for n

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,15 +1,17 @@
 import time
 
+n = 100
+
 
 def not_using_mod():
     start_time = time.time()
     three = 3
     five = 5
-    result = list(range(1, 101))
-    while three <= 100:
+    result = list(range(1, n + 1))
+    while three <= n:
         result[three - 1] = 'Fizz'
         three += 3
-    while five <= 100:
+    while five <= n:
         result[five - 1] = 'FizzBuzz' if result[five - 1] == 'Fizz' else 'Buzz'
         five += 5
 
