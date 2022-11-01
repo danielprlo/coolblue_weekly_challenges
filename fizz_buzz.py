5d91e783a92fb440ed3fad172bb5b5fa3a16d9cd
Dani <daniel.perez@coolblue.nl>
2022-11-01 09:40:12 +0100

    FizzBuzz added

---
+++ fizz_buzz.py
@@ -0,0 +1,21 @@
+import time
+
+
+def not_using_mod():
+    start_time = time.time()
+    three = 3
+    five = 5
+    result = list(range(1, 100))
+    while three < 100:
+        result[three - 1] = 'Fizz'
+        three += 3
+    while five < 100:
+        result[five - 1] = 'FizzBuzz' if result[five - 1] == 'Fizz' else 'Buzz'
+        five += 5
+
+    print(result)
+    final_time = time.time() - start_time
+    print(final_time * 1000)
+
+
+not_using_mod()
